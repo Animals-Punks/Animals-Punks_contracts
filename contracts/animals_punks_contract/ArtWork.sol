e5417967ca16ec8b012af6e39a2673f371cbc8eb
mo-zza <bigsmalljoe@gmail.com>
2022-01-07 17:04:21 +0900

    feat: Initial commit

---
+++ contracts/animals_punks_contract/ArtWork.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+import "../token/KIP37/KIP37.sol";
+import "../token/KIP37/KIP37Burnable.sol";
+import "../token/KIP37/KIP37Pausable.sol";
+import "../token/KIP37/KIP37Mintable.sol";
+
+contract KIP37Token is KIP37, KIP37Burnable, KIP37Pausable, KIP37Mintable {
+    constructor(string memory uri) public KIP37(uri) {}
+}
