f02382de80a3b65d532d3380cdcc3673fca75c8f
mo-zza <bigsmalljoe@gmail.com>
2022-01-08 17:26:22 +0900

    feat: owner & set uri

--- contracts/animals_punks_contract/ArtWork.sol
+++ contracts/animals_punks_contract/ArtWork.sol
@@ -4,7 +4,8 @@ import "../token/KIP37/KIP37.sol";
 import "../token/KIP37/KIP37Burnable.sol";
 import "../token/KIP37/KIP37Pausable.sol";
 import "../token/KIP37/KIP37Mintable.sol";
+import "../ownership/Ownable.sol"; 
 
-contract AnimalsPunksArtWork is KIP37, KIP37Burnable, KIP37Pausable, KIP37Mintable {
+contract AnimalsPunksArtWork is KIP37, KIP37Burnable, KIP37Pausable, KIP37Mintable, Ownable {
     constructor(string memory uri) public KIP37(uri) {}
 }
